9d287abb2748ca35110e9905fd39532ad6ed1ae9
Kirbstomper <wingedkraby@gmail.com>
2018-11-19 18:52:29 -0500

    Fixed retreat cooldown, added in basic display for peek time and cooldown time

--- Assets/Scripts/BoxRef.cs
+++ Assets/Scripts/BoxRef.cs
@@ -8,29 +8,29 @@ public class BoxRef : MonoBehaviour {
     const float COOLDOWN_TIME = 6.0f;
     const float RETREAT_TIME = 2.0f;
     //The actual boxes. will be passed
-    public BoxPeekPlr player_1;
-    public BoxPeekPlr player_2;
+    public BoxPeekPlr player_one;
+    public BoxPeekPlr player_two;
 
     //Players time peeking, really basic
-    private float player_1_peek_time;
-    private float player_2_peek_time;
+    private float player_one_peek_time;
+    private float player_two_peek_time;
     // Player peek booleans, will control logic and start the counter
-    private bool player_1_is_peek;
-    private bool player_2_is_peek;
+    private bool player_one_is_peek;
+    private bool player_two_is_peek;
     //Player peek cooldown times after retreating from a peek.
-    private float player_1_cooldown;
-    private float player_2_cooldown;
+    private float player_one_cooldown;
+    private float player_two_cooldown;
     // This enum determines the game status.
     public enum game_states {NO_WIN = 0,
-                            PLAYER_1 = 1,
-                            PLAYER_2 = 2,
+                            player_one = 1,
+                            player_two = 2,
                             GAME_OVER = 3}
     private int game_status = 0;
     // Use this for initialization
     void Start () {
 
-        player_1_peek_time = player_2_peek_time = 0.0f;
-        player_1_is_peek = player_2_is_peek = false;
+        player_one_peek_time = player_two_peek_time = 0.0f;
+        player_one_is_peek = player_two_is_peek = false;
         Time.timeScale = 1;
     
 	}
@@ -45,33 +45,33 @@ public class BoxRef : MonoBehaviour {
             if (game_status != (int)game_states.NO_WIN)
             {
                 print("Game ended due to counter peek! Player " + game_status + "Wins!");
-                player_1 = player_2 = null;// Causes a null pointer error prob
+                player_one = player_two = null;// Causes a null pointer error prob
             }
 
             //Otherwise increment whoever is peeking and check if someone has won!
             else
             {
                 //Cooldown time tickdown
-                if (player_1_cooldown > 0.0f) player_1_cooldown -= Time.deltaTime;
-                if (player_2_cooldown > 0.0f) player_2_cooldown -= Time.deltaTime;
+                if (player_one_cooldown > 0) player_one_cooldown -= Time.deltaTime;
+                if (player_two_cooldown > 0) player_two_cooldown -= Time.deltaTime;
 
                 //Peek Time for if a player is peeking
-                if (player_1_is_peek) player_1_peek_time += Time.deltaTime;
-                if (player_2_is_peek) player_2_peek_time += Time.deltaTime;
+                if (player_one_is_peek) player_one_peek_time += Time.deltaTime;
+                if (player_two_is_peek) player_two_peek_time += Time.deltaTime;
 
-                if (player_1_peek_time >= COMPLETE_PEEK_TIME)
+                if (player_one_peek_time >= COMPLETE_PEEK_TIME)
                 {
-                    game_status = (int)game_states.PLAYER_1;
+                    game_status = (int)game_states.player_one;
                     print("Game ended due complete peek! Player " + game_status + "Wins!");
-                    player_1 = player_2 = null;// Causes a null pointer error prob
+                    player_one = player_two = null;// Causes a null pointer error prob
                     game_status = (int)game_states.GAME_OVER;
 
                 }
-                if (player_2_peek_time >= COMPLETE_PEEK_TIME)
+                if (player_two_peek_time >= COMPLETE_PEEK_TIME)
                 {
-                    game_status = (int)game_states.PLAYER_2;
+                    game_status = (int)game_states.player_two;
                     print("Game ended due complete peek! Player " + game_status + "Wins!");
-                    player_1 = player_2 = null;// Causes a null pointer error prob
+                    player_one = player_two = null;// Causes a null pointer error prob
                     game_status = (int)game_states.GAME_OVER;
                 }
             }
@@ -92,20 +92,20 @@ public class BoxRef : MonoBehaviour {
 
      public bool GetInput(BoxPeekPlr plr)
     {
-        if(plr == player_1)
+        if(plr == player_one)
         {
-            if(player_1_cooldown == 0.0f)
+            if(player_one_cooldown <= 0.0f)
             {
-                if (!player_1_is_peek) return Peek(plr);
+                if (!player_one_is_peek) return Peek(plr);
                 else { return StopPeek(plr); }
             }
             
         }
-        if (plr == player_2)
+        if (plr == player_two)
         {
-            if (player_2_cooldown == 0.0f)
+            if (player_two_cooldown <= 0.0f)
             {
-                if (!player_2_is_peek) return Peek(plr);
+                if (!player_two_is_peek) return Peek(plr);
                 else { return StopPeek(plr); }
             }
           
@@ -119,24 +119,24 @@ public class BoxRef : MonoBehaviour {
      * **/
     public bool Peek(BoxPeekPlr plr)
     {
-        if(plr == player_1 )
+        if(plr == player_one )
         {
-            if (player_2_is_peek)
+            if (player_two_is_peek)
             {
-                game_status = (int)game_states.PLAYER_1;
+                game_status = (int)game_states.player_one;
                 return true;
             }
-            player_1_is_peek = true;
+            player_one_is_peek = true;
             return true;
         }
-        if(plr == player_2)
+        if(plr == player_two)
         {
-            if (player_1_is_peek)
+            if (player_one_is_peek)
             {
-                game_status = (int)game_states.PLAYER_2;
+                game_status = (int)game_states.player_two;
                 return true;
             }
-            player_2_is_peek = true;
+            player_two_is_peek = true;
             return true;
         }
         return false;
@@ -148,23 +148,47 @@ public class BoxRef : MonoBehaviour {
      * */
     public bool StopPeek(BoxPeekPlr plr)
     {
-        if(plr == player_1 && player_1_peek_time >= RETREAT_TIME)
+        if(plr == player_one && player_one_peek_time >= RETREAT_TIME)
         {
-            player_1_is_peek = false;
-            player_1_peek_time = 0.0f;
-            player_1_cooldown = COOLDOWN_TIME;
+            player_one_is_peek = false;
+            player_one_peek_time = 0.0f;
+            player_one_cooldown = COOLDOWN_TIME;
             return true;
         }
-        if (plr == player_2 && player_2_peek_time >= RETREAT_TIME)
+        if (plr == player_two && player_two_peek_time >= RETREAT_TIME)
         {
-            player_2_is_peek = false;
-            player_2_peek_time = 0.0f;
-            player_2_cooldown = COOLDOWN_TIME;
+            player_two_is_peek = false;
+            player_two_peek_time = 0.0f;
+            player_two_cooldown = COOLDOWN_TIME;
             return true;
         }
         return false;
 
     }
 
+    public float GetPlayerOnePeekTime()
+    {
 
+        return player_one_peek_time;
+
+    }
+
+    public float GetPlayerTwoPeekTime()
+    {
+        return player_two_peek_time;
+    }
+
+    public float GetPlayerOneCooldown()
+    {
+        return player_one_cooldown;
+    }
+    public float GetPlayerTwoCooldown()
+    {
+        return player_two_cooldown;
+    }
+
+    public int GetGameStatus()
+    {
+        return game_status;
+    }
 }
--- Assets/Scripts/BoxRefTextPresentor.cs
+++ Assets/Scripts/BoxRefTextPresentor.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoxRefTextPresentor : MonoBehaviour {
+    public BoxRef box_ref;
+    public Text player_one_peek_time;
+    public Text player_two_peek_time;
+    public Text player_one_cooldown;
+    public Text player_two_cooldown;
+   
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +18,13 @@ public class BoxRefTextPresentor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        player_one_peek_time.text =  box_ref.GetPlayerOnePeekTime().ToString(); 
+        player_two_peek_time.text = box_ref.GetPlayerTwoPeekTime().ToString();
+
+        player_one_cooldown.text = box_ref.GetPlayerOneCooldown().ToString();
+        player_two_cooldown.text = box_ref.GetPlayerTwoCooldown().ToString();
+
+       
+    }
 }
